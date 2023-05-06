b7b2b6db99c54bf1acee32203bda2d094fb54106
Rizka Azmira <cika@Rizkas-MacBook-Air.local>
2023-05-06 17:30:34 +0700

    Add forms

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<form name="will-attend" method="POST" netlify-honeypot="bot-field" data-netlify="true">
+    <input type="hidden" name="form-name" value="will-attend" />
+    <label for="name">Name</label>
+    <input name="name" id="name" required placeholder="Name" type="text" />
+    <label for="email">Email</label>
+    <input name="email" id="email" required placeholder="Email" type="email" />
+    <label for="message">Message</label>
+    <input name="message" id="message" required placeholder="Message" type="text" />
+    <input type="submit" value="Submit" />
+  </form>
+
+
+  <form name="will-not-attend" method="POST" netlify-honeypot="bot-field" data-netlify="true">
+    <input type="hidden" name="form-name" value="will-not-attend" />
+    <label for="name">Name</label>
+    <input name="name" id="name" required placeholder="Name" type="text" />
+    <label for="email">Email</label>
+    <input name="email" id="email" required placeholder="Email" type="email" />
+    <label for="message">Message</label>
+    <input name="message" id="message" required placeholder="Message" type="text" />
+    <input type="submit" value="Submit" />
+  </form>
